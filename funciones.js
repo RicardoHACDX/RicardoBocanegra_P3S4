a2f22240f0f848034d197478a10d57670dbc6a3d
RicardoBocanegra <ricardomendez145@gmail.com>
2020-12-10 16:55:40 -0600

    funcion

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function mostrarPersona(datos){
+    const nombre=datos.results[0].name
+    const foto=datos.results[0].picture.large
+
+    const divPersona = document.querySelector("#persona")
+
+    divPersona.innerHTML = $; {nombre.first} $; {nombre.last}
+    }
+    fetch('https://randomuser.me/api/')
+    datatype:(JSON),
+    éxito; función(datos); 
+    {
+      console.log ( datos );
+    }
+
+        
